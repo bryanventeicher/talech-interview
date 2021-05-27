e836a4b464c6a8d386149408cb01a29954ca9345
bryanventeicher <bryanlventeicher@gmail.com>
2021-05-27 15:30:02 -0600

    Update User.java

--- src/main/java/com/talech/interview/User.java
+++ src/main/java/com/talech/interview/User.java
@@ -11,16 +11,32 @@ public class User {
 		return id;
 	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getUsername() {
 		return username;
 	}
 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
 	public String getEmail() {
 		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
